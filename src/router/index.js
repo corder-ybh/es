2b00fffd7637c0ad71e757e5cdec1bf2eafc6320
bhyang <2270933604@qq.com>
2019-11-18 16:35:20 +0800

    不太好用

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/pages/Login'
 
-Vue.use(Router)
-
+Vue.use(Router);
+function route (path, file, name, children) {
+  return {
+    exact: true,
+    path,
+    name,
+    children,
+    component: () => import('../pages' + file)
+  }
+}
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Login',
-      component: Login
-    }
+
+    route("/login",'/Login',"Login"),
+    route('/index', '/Layout', 'index'),
   ]
 })
