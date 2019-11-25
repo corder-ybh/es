776658668b4a47dc63762adf03161cb8d2703f66
bhyang <2270933604@qq.com>
2019-11-25 15:25:07 +0800

    tree修改成功

--- src/views/tree/index.vue
+++ src/views/tree/index.vue
@@ -15,49 +15,17 @@
 </template>
 
 <script>
+import { getCategoryTree } from '@/api/category_tree'
+
 export default {
 
   data() {
     return {
       filterText: '',
-      data2: [{
-        id: 1,
-        label: 'Level one 1',
-        children: [{
-          id: 4,
-          label: 'Level two 1-1',
-          children: [{
-            id: 9,
-            label: 'Level three 1-1-1'
-          }, {
-            id: 10,
-            label: 'Level three 1-1-2'
-          }]
-        }]
-      }, {
-        id: 2,
-        label: 'Level one 2',
-        children: [{
-          id: 5,
-          label: 'Level two 2-1'
-        }, {
-          id: 6,
-          label: 'Level two 2-2'
-        }]
-      }, {
-        id: 3,
-        label: 'Level one 3',
-        children: [{
-          id: 7,
-          label: 'Level two 3-1'
-        }, {
-          id: 8,
-          label: 'Level two 3-2'
-        }]
-      }],
+      data2: [],
       defaultProps: {
         children: 'children',
-        label: 'label'
+        label: 'name'
       }
     }
   },
@@ -66,8 +34,16 @@ export default {
       this.$refs.tree2.filter(val)
     }
   },
-
+  created() {
+    this.fetchData()
+  },
   methods: {
+    fetchData() {
+      getCategoryTree().then(response => {
+        console.log(response)
+        this.data2 = response.tree
+      })
+    },
     filterNode(value, data) {
       if (!value) return true
       return data.label.indexOf(value) !== -1
